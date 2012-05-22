3cdbe41c63564d0a4ae722d0a807d7627ddd1e2a
pthompson <pete@hobokenrecorders.com>
2012-05-22 18:44:32 -0400

    added clock function for current time in user's location

--- js/main.js
+++ js/main.js
@@ -7,3 +7,32 @@
 
 
 
+function startTime()
+{
+var today=new Date();
+var h=today.getHours();
+var m=today.getMinutes();
+var s=today.getSeconds();
+// add a zero in front of numbers<10
+m=checkTime(m);
+s=checkTime(s);
+if (h > 12) {
+      h -= 12;
+		}
+		else if (hours == 0) {
+      hours = 12;
+   }
+
+document.getElementById('time').innerHTML=h+":"+m+":"+s;
+t=setTimeout('startTime()',500);
+}
+
+function checkTime(i)
+{
+if (i<10)
+  {
+  i="0" + i;
+  }
+return i;
+}
+startTime();
