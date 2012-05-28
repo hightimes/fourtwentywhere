8fcad30fe1574ac3901bdd0d80c1b8e96eb5610d
pthompson <pete@hobokenrecorders.com>
2012-05-28 14:57:18 -0400

    improvements to donut finder

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,23 +1,18 @@
 var geocoder;
   var map;
 	var service;
-  function initialize() {
+	var infowindow;
+
+	
+  function codeAddress() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(-34.397, 150.644);
     var myOptions = {
-      zoom: 15,
-      center: latlng,
+      zoom: 13,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-			
     }
 		
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-		
-	 
-  }
-	
-	
-  function codeAddress() {
 		
 		
     var address = document.getElementById("address").value;
@@ -30,11 +25,11 @@ var geocoder;
             position: results[0].geometry.location
         });
       } else {
-        alert("Geocode was not successful for the following reason: " + status);
+        alert("No donuts for you: please enter your zip code or city and try again");
       }
 		  var request = {
 		    location: results[0].geometry.location,
-		    radius: '500',
+		    radius: '4000',
 		    keyword: ['donuts']
 		  };
 			
@@ -46,7 +41,7 @@ var geocoder;
 			    }
 			  }
 			}
-	
+			infowindow = new google.maps.InfoWindow();
 			service = new google.maps.places.PlacesService(map);
 			service.search(request, callback);
 			
@@ -58,5 +53,11 @@ var geocoder;
 		      map: map,
 		      position: place.geometry.location
 		    });
+				google.maps.event.addListener(marker, 'click', function() {
+				          infowindow.setContent(place.name);
+				          infowindow.open(map, this);
+				        });
+				
 			}
+			
   }
